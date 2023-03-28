25c2bdae168d611690b969f2cc5dd6d023bb5eea
pafg89 <61020253+pafg89@users.noreply.github.com>
2023-03-28 11:36:39 +0200

    Create mperature-raspberry.sh

---
+++ mperature-raspberry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
+ echo "Equipo => $(hostname)"
+ echo "$(date)"
+ echo "------------------------------"
+ echo "Temp.CPU => $((cpu/1000))'Cº"
+ echo "Temp.GPU => $(/usr/bin/vcgencmd measure_temp)"
+ echo "------------------------------"
+ echo "CPU"
+ echo "$(vcgencmd measure_volts core)"
+ echo "Mem. del Sistema $(vcgencmd get_mem arm)"
+ echo "Mem. de la $(vcgencmd get_mem gpu)"
+ echo "------------------------------"
+ echo "Consumo de memoria"
+ echo "$(egrep --color 'Mem|Cache|Swap' /proc/meminfo)"
