79114af9bd10ae02862944f7ed097fb2c066ebc3
pafg89 <61020253+pafg89@users.noreply.github.com>
2023-05-09 16:56:10 +0200

    Update backup_database.sh
    The MySQL user validation has been improved.

--- backup_database.sh
+++ backup_database.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
-# Configure your MySQL user
-MYSQL_USER="your_user"
+# Ask the user for the MySQL user
+read -p "Please enter your MySQL user: " mysql_user
 
 # Ask the user for the MySQL password
-echo "Please enter your MySQL password:"
-read -s MYSQL_PASSWORD
+read -s -p "Please enter your MySQL password: " mysql_password
+echo
+
+# Check if the MySQL user is valid
+mysql --user="$mysql_user" --password="$mysql_password" --execute="SELECT 1;" 2>/dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Invalid MySQL user or password. Please check your credentials. Exiting."
+    exit 1
+fi
 
 # Get the list of databases
-databases=$(mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" 2>/dev/null | grep -Ev "Database|information_schema|performance_schema")
+databases=$(mysql --user="$mysql_user" --password="$mysql_password" --execute="SHOW DATABASES;" 2>/dev/null | grep -Ev "Database|information_schema|performance_schema")
 
 # Verify if the connection was successful
 if [ -z "$databases" ]; then
-  echo "Error connecting to MySQL. Please check your username and password. Exiting."
-  exit 1
+    echo "Error connecting to MySQL. Please check your username and password. Exiting."
+    exit 1
 fi
 
 # Display the databases with an associated number
 echo "List of databases:"
+db_map=()
 counter=1
-declare -A db_map
 for db in $databases; do
-  echo "$counter - $db"
-  db_map[$counter]=$db
-  counter=$((counter+1))
+    echo "$counter - $db"
+    db_map+=("$db")
+    counter=$((counter+1))
 done
 
 # Ask the user to select a database
-echo "Please select the database you want to backup (enter the associated number):"
-read selected_db_number
+read -p "Please select the database you want to backup (enter the associated number): " selected_db_number
 
 # Verify if the selected number is valid
-if [[ -z "${db_map[$selected_db_number]}" ]]; then
-  echo "Invalid number. Exiting."
-  exit 1
+if [ -z "${db_map[selected_db_number-1]}" ]; then
+    echo "Invalid number. Exiting."
+    exit 1
 fi
 
 # Perform the backup of the selected database
-selected_db="${db_map[$selected_db_number]}"
-backup_file="${selected_db}_$(date +%Y%m%d_%H%M%S).sql"
-mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD "$selected_db" > "$backup_file"
+selected_db="${db_map[selected_db_number-1]}"
+backup_file="${selected_db}_$(date +'%Y%m%d_%H%M%S').sql"
+mysqldump --user="$mysql_user" --password="$mysql_password" "$selected_db" > "$backup_file"
 
 # Check if the backup was successful
 if [ $? -eq 0 ]; then
-  echo "Backup of database '$selected_db' was successful. File: $backup_file"
+    echo "Backup of database '$selected_db' was successful. File: $backup_file"
 else
-  echo "Error while backing up database '$selected_db'."
+    echo "Error while backing up database '$selected_db'."
 fi
